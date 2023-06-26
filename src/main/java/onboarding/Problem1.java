99ef8f66ce299f77d99c258af98061260724225b
Kim hyemin <59169881+hyee0715@users.noreply.github.com>
2023-06-26 22:50:36 +0900

    Revert "feat(Problem1.java): 최종 점수를 비교하여 결과 반환"
    This reverts commit 36b01ade6ed372487b080fdc67eba5446b0f7bc7.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,18 +16,6 @@ class Problem1 {
         int pobiFinalScore = getFinalScore(pobi);
         int crongFinalScore = getFinalScore(crong);
 
-        if (pobiFinalScore > crongFinalScore) {
-            answer = 1;
-        }
-
-        if (pobiFinalScore < crongFinalScore) {
-            answer = 2;
-        }
-
-        if (pobiFinalScore == crongFinalScore){
-            answer = 0;
-        }
-
         return answer;
     }
 
